1301421e67cf1af46f8248e2f210332b41e5ffed
D3FVLT <whitenobel0@gmail.com>
2022-06-10 14:58:30 +0300

    sms fix

--- src/controllers/verificationController.ts
+++ src/controllers/verificationController.ts
@@ -5,6 +5,7 @@ export async function sendSMS(verification_code: number, phone_number: number) {
   const sendResult = await smsRu.sendSms({
     to: `${phone_number}`,
     msg: `Ваш проверочный код: ${verification_code}`,
+    from: '79265990995',
   });
   console.log(sendResult);
   return;
